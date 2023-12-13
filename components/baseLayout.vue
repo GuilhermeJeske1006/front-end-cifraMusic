ee7b6054e9772d985ad2d87e3ff68206fdd382dd
Guilherme jeske <guilhermeieski@gmail.com>
2023-12-13 10:56:13 -0300

    start project

---
+++ components/baseLayout.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+      <static-navbar/>
+      <main>
+        <slot></slot>
+    </main>
+    <static-footer/>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'BaseLayout'
+  }
+  </script>
+  
---
+++ pages/index.vue
@@ -0,0 +1,26 @@
+<template>
+ <base-layout>
+ <static-header/>
+ <div class="mx-auto max-w-7xl sm:py-12 sm:px-6 lg:px-8">
+
+  <div class="bg-white sm:py-12">
+  <div class="mx-auto max-w-7xl px-6 lg:px-8">
+    <div class="mx-auto max-w-2xl lg:text-center">
+      <h2 class="text-base font-semibold leading-7 text-indigo-600">Deploy faster</h2>
+      <p class="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Everything you need to deploy your app</p>
+      <p class="mt-6 text-lg leading-8 text-gray-600">Quis tellus eget adipiscing convallis sit sit eget aliquet quis. Suspendisse eget egestas a elementum pulvinar et feugiat blandit at. In mi viverra elit nunc.</p>
+    </div>
+  </div>
+</div>
+
+  <general-list/>
+  <general-paginate/>
+ </div>
+  </base-layout>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage'
+}
+</script>
