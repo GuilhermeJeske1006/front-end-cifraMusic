863e85cb078c5be5502c0017287427857fc8c1af
Guilherme jeske <guilhermeieski@gmail.com>
2024-01-09 11:05:57 -0300

    front-13: add options on the music listing page

--- pages/music/listing.vue
+++ pages/music/listing.vue
@@ -4,13 +4,14 @@
 
     <div class="mx-auto max-w-7xl mb-20 sm:py-3 sm:px-6 lg:px-8">
       <div class="bg-white sm:py-12">
-          <div class="mx-auto max-w-7xl px-6 lg:px-8">
-            <div class="mx-auto max-w-2xl lg:text-center">
-              <p class="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Listagem de todas as músicas</p>
-              <p class="mt-6 text-lg leading-8 text-gray-600">Encontre a sua música abaixo, você pode fazer a pesquisa pelo nome da música ou filtrar através do artista ou ritmo da mesma.</p>
-            </div>
+        <div class="mx-auto max-w-7xl px-6 lg:px-8">
+          <div class="mx-auto max-w-2xl lg:text-center">
+            <p class="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Listagem de todas as músicas</p>
+            <p class="mt-6 text-lg leading-8 text-gray-600">Encontre a sua música abaixo, você pode fazer a pesquisa pelo
+              nome da música ou filtrar através do artista ou ritmo da mesma.</p>
           </div>
         </div>
+      </div>
 
       <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
         <div class="hidden shrink-0 mb-7  sm:flex sm:flex-col sm:items-start">
@@ -47,13 +48,13 @@
           <h2 class="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Contact sales</h2>
 
         </div>
-        <music-list-music />
+        <music-list-music :items="people" :setting="setting" />
 
         <div class="relative mx-auto mt-10 w-full max-w-container ">
           <h2 class="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Contact sales</h2>
 
         </div>
-        <music-list-music />
+        <music-list-music :items="people" :setting="setting" />
 
         <general-paginate />
       </div>
@@ -63,6 +64,8 @@
 </template>
   
 <script setup>
+import { ref } from "vue";
+
 
 const tabs = [
   {
@@ -88,6 +91,75 @@ const tabs = [
 
   }
 ]
+
+const setting = ref([
+  {
+    name: 'Ver',
+    action: ''
+  },
+  {
+    name: 'Editar',
+    action: ''
+  },
+  {
+    name: 'Excluir',
+    action: ''
+  },
+])
+
+const people = ref([
+  {
+    name: 'Leslie Alexander',
+    email: 'leslie.alexander@example.com',
+    role: 'Co-Founder / CEO',
+    imageUrl:
+      'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+    lastSeen: '3h ago',
+    lastSeenDateTime: '2023-01-23T13:23Z',
+  },
+  {
+    name: 'Michael Foster',
+    email: 'michael.foster@example.com',
+    role: 'Co-Founder / CTO',
+    imageUrl:
+      'https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+    lastSeen: '3h ago',
+    lastSeenDateTime: '2023-01-23T13:23Z',
+  },
+  {
+    name: 'Dries Vincent',
+    email: 'dries.vincent@example.com',
+    role: 'Business Relations',
+    imageUrl:
+      'https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+    lastSeen: null,
+  },
+  {
+    name: 'Lindsay Walton',
+    email: 'lindsay.walton@example.com',
+    role: 'Front-end Developer',
+    imageUrl:
+      'https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+    lastSeen: '3h ago',
+    lastSeenDateTime: '2023-01-23T13:23Z',
+  },
+  {
+    name: 'Courtney Henry',
+    email: 'courtney.henry@example.com',
+    role: 'Designer',
+    imageUrl:
+      'https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+    lastSeen: '3h ago',
+    lastSeenDateTime: '2023-01-23T13:23Z',
+  },
+  {
+    name: 'Tom Cook',
+    email: 'tom.cook@example.com',
+    role: 'Director of Product',
+    imageUrl:
+      'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+    lastSeen: null,
+  },]);
 
 </script>
   
