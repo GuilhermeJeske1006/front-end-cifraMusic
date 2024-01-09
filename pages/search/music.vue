766a33a049f37143337d9492ca20be7ff6b26394
Guilherme jeske <guilhermeieski@gmail.com>
2024-01-09 09:04:32 -0300

    front-13: finally page of the search a new song

---
+++ pages/search/music.vue
@@ -0,0 +1,69 @@
+<template>
+    <base-layout>
+      <static-header :item="'Buscar Música ou Artista'" />
+  
+      <div class="mx-auto max-w-7xl mb-20 sm:py-3 sm:px-6 lg:px-8">
+        <div class="bg-white sm:py-12">
+            <div class="mx-auto max-w-7xl px-6 lg:px-8">
+              <div class="mx-auto max-w-2xl lg:text-center">
+                <p class="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Busque uma nova música</p>
+                <p class="mt-6 text-lg leading-8 text-gray-600">Encontre a sua música abaixo, você pode fazer a pesquisa pelo nome da música ou filtrar através do artista ou ritmo da mesma.</p>
+              </div>
+            </div>
+          </div>
+  
+        <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
+         
+          <general-tabs :items="tabs" />
+
+          <general-filter class="mb-5">
+            <div class="relative w-full ">
+              <div class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+                <svg class="w-4 h-4 text-gray-500 " aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
+                  viewBox="0 0 20 20">
+                  <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                    d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
+                </svg>
+              </div>
+              <input type="text" id="simple-search"
+                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5  "
+                placeholder="Pesquise pelo nome da música..." required>
+            </div>
+          </general-filter>
+
+  
+          <music-list-music />
+  
+          <general-paginate />
+        </div>
+  
+      </div>
+    </base-layout>
+  </template>
+    
+    <script setup>
+  
+    const tabs = [
+      {
+        icon: ` <svg class="w-4 h-4 me-2 text-gray-400 group-hover:text-gray-500 dark:text-gray-500 dark:group-hover:text-gray-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 10">
+        <path stroke="currentColor" stroke-linecap="round" stroke-width="2" d="M6 1h10M6 5h10M6 9h10M1.49 1h.01m-.01 4h.01m-.01 4h.01"/>
+      </svg>`,
+        name: 'Por Música',
+        href: '/search/music'
+        
+    
+      },
+      {
+        icon: `<svg class="w-4 h-4 me-2 text-gray-400 group-hover:text-gray-500 dark:text-gray-500 dark:group-hover:text-gray-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 16 19">
+        <path d="M15 5a1 1 0 0 0-1 1v3a4 4 0 0 1-4 4H6a4 4 0 0 1-4-4V6a1 1 0 0 0-2 0v3a6.006 6.006 0 0 0 6 6h1v2H5a1 1 0 0 0 0 2h6a1 1 0 0 0 0-2H9v-2h1a6.006 6.006 0 0 0 6-6V6a1 1 0 0 0-1-1Z"/>
+        <path d="M9 0H7a3 3 0 0 0-3 3v5a3 3 0 0 0 3 3h2a3 3 0 0 0 3-3V3a3 3 0 0 0-3-3Z"/>
+      </svg>`,
+        name: 'Por Artista',
+        href: '/search/artist'
+  
+    
+      }
+    ]
+    
+    </script>
+    
