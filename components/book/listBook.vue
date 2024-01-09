8840e483308329d9b3f2f5e6ed6e740dcb63727e
Guilherme jeske <guilhermeieski@gmail.com>
2024-01-09 10:52:41 -0300

    front-13: change modal position

--- components/book/listBook.vue
+++ components/book/listBook.vue
@@ -25,7 +25,7 @@
 
 
                 <div v-if="showSettings[index]"
-                    class="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
+                    class="absolute  z-10 mt-2 w-48  rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
                     role="menu" aria-orientation="vertical" :aria-labelledby="'user-menu-button-' + index" tabindex="-1">
                     
                     <a v-for="item in setting" :key="item.id" :href="item.action" class="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1"
