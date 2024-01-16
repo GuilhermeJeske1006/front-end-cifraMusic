10c0f2ca0df3bd1ba0fb948dcf8b4aeca26a30da
guilherme <guilhermeieski@gmail.com>
2024-01-16 16:16:37 -0300

    front-15: Creates a screen for music details

---
+++ pages/music/detail/_id.vue
@@ -0,0 +1,38 @@
+<template>
+  <base-layout>
+    <static-header :item="item.title" />
+
+    <div class="mx-auto max-w-7xl sm:py-6 sm:px-6 lg:px-8">
+      <div class="bg-white ">
+        <div class="mx-auto max-w-7xl  sm:px-6 lg:px-8">
+          <p class="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">{{ item.title }}</p>
+          <p class="mt-3 text-lg leading-8 text-gray-600">{{item.singer}}</p>
+          <p class="mt-3 text-lg leading-8 text-gray-600">Nota: {{item.note}} - BPM: {{item.bpm}}</p>
+
+        </div>
+
+      </div>
+      <div class=" max-w-7xl sm:py-12 sm:px-6 lg:px-8">
+        <p class="mt-6 text-lg leading-8 text-gray-600">{{item.lyrics}}</p>
+      </div>
+
+    </div>
+  </base-layout>
+</template>
+
+<script setup>
+
+
+const item =
+  {
+    singer: 'Aline Barros',
+    lyrics: 'Sonda-me, Senhor, e me conhece Quebranta o meu coração Transforma-me conforme a Tua palavra E enche-me até que em mim',
+    rhythm: 'Gospel',
+    title: 'Sonda-me, Usa-me',
+    bpm: '120',
+    note: 'Dó',
+
+  }
+
+</script>
+
